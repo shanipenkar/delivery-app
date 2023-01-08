529ca6b8df761f791bffdd330ce9db109d413df2
Shani Penkar <shanipenkar@gmail.com>
2023-01-08 11:31:23 +0200

    add screens folder

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+///** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+  "./screens/**/*.{js,jsx,ts,tsx}",
+  "./components/**/*.{js,jsx,ts,tsx}",
+  "./pages/**/*.{js,jsx,ts,tsx}"],
+};
