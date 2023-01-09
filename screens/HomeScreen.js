d415321ea0de55e581c8e1e24380d5f5e9008a5f
Shani Penkar <shanipenkar@gmail.com>
2023-01-09 13:12:54 +0200

    added scroll-views to homescreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,6 +3,9 @@ import { View, Text, SafeAreaView, Image, TextInput } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import * as Icons from "react-native-heroicons/outline";
+import { ScrollView } from "react-native";
+import Categories from "../components/Categories.js";
+import FeaturedRow from "../components/FeaturedRow.js";
 
 const HomeScreen = () => {
   const navigation = useNavigation(); // access to the navigation object
@@ -14,7 +17,6 @@ const HomeScreen = () => {
   return (
     // SafeAreaView is a component that makes sure that the content is not hidden behind the status bar
     <SafeAreaView className="bg-white pt-5">
-      <Text>
         {/* Header */}
         <View className="flex-row pb-3 items-center mx-4 space-x-2 px-4">
           <Image
@@ -23,8 +25,7 @@ const HomeScreen = () => {
             }}
             className="h-7 w-7 bg-gray-300 p-4 rounded-full"
           />
-          {/* flex-1 will make the view  take the most of the place and push the other to the sides (put the user icon on the right side) */}
-          <View className="">
+          <View className="flex-1">
             <Text className="font-bold text-grey-400 text-xs">
               Deliver Now!
             </Text>
@@ -37,14 +38,42 @@ const HomeScreen = () => {
         </View>
 
         {/* Search */}
-        <View className="flex-row items-center space-x-2 pb-2 mx-4 px-4">
+        <View className="flex-row items-center space-x-2 pb-2 mx-4">
           <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
             <Icons.MagnifyingGlassIcon color="gray" size={20} />
             <TextInput placeholder="Search Restaurants..." keyboardType="default" />
           </View>
           <Icons.AdjustmentsVerticalIcon color="#00CCBB" />
         </View>
-      </Text>
+
+        {/* Body */}
+        <ScrollView className="bg-gray-100"
+        contentContainerStyle={{
+          paddingBottom: 100,
+        }}>
+
+          {/* Categories */}
+          <Categories/>
+
+          {/* Features Rows */}
+          <FeaturedRow
+          id="123"
+          title="Featured"
+          description="Paid placement from our partners"
+          />
+          <FeaturedRow
+          id="1234"
+          title="Tasty Discounts"
+          description="Everyone's been enjoying these juicy discounts!"
+          />
+          <FeaturedRow
+          id="12345"
+          title="Offers near you!"
+          description="Why not support your local restaurant tonight!"
+          />
+          
+
+        </ScrollView>
     </SafeAreaView>
   );
 };
