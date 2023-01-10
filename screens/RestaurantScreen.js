ba3a362a7e39584fec0f124a8a1a64638b677040
Shani Penkar <shanipenkar@gmail.com>
2023-01-10 15:48:57 +0200

    navigation added from homescreen to restaurantscreen

---
+++ screens/RestaurantScreen.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { useRoute } from '@react-navigation/native'
+
+const RestaurantScreen = () => {
+    
+  return (
+    <View>
+      <Text>RestaurantScreen</Text>
+    </View>
+  )
+}
+
+export default RestaurantScreen;
